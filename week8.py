31d0894d112f539e9c81310fa49bc86e35080944
nasyaarphelia <nasyaarphelia11@gmail.com>
2022-08-13 12:50:26 +0700

    menambahkan tugas 8

---
+++ week8.py
@@ -0,0 +1,30 @@
+import pymongo
+import datetime
+from flask import Flask,request
+
+app = Flask(__name__)
+
+client = pymongo.MongoClient("mongodb+srv://restect:pastibisa123@restect.8l6tv0l.mongodb.net/?retryWrites=true&w=majority")
+db = client['restect']
+my_collections = db['week8']
+
+@app.route('/location',methods = ['GET','POST'])
+def location():
+    kecepatan = request.args.get('kecepatan')
+    latitude = request.args.get('latitude')
+    longitude = request.args.get('longitude')
+
+    if request.method == 'POST' :
+        result = my_collections.insert_one({"kecepatan":kecepatan,"latitude":latitude,"longitude":longitude})
+        print(result)
+        return {
+            "output":{
+                "kecepatan": kecepatan,
+                "latitude":latitude,
+                "longitude":longitude,
+                "timestamp":datetime.datetime.now()
+                    }
+            }
+
+if __name__ == '__main__':
+    app.run(debug=True)
